16ea0beaa8fd68bd106aee0e2301778d4d84743b
cdrawlings <c.d.rawlings@gmail.com>
2021-11-01 10:31:59 -0400

    Updateed address and such for Heroku

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,7 @@ module.exports = function (passport) {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: 'http://localhost:3000/auth/google/callback',
+                callbackURL: 'https://bird-nerd-counter.herokuapp.com/auth/google/callback',
             },
             async (accessToken, refreshToken, profile, done) => {
                 const newUser = {
